d07c49cdc5918c4d62b2da5ccf80f768a475eee7
Jed Chen <jedc101@attu2.cs.washington.edu>
2019-02-26 17:02:46 -0800

    Added Test functionality

---
+++ src/test/java/ExampleTests.java
@@ -0,0 +1,20 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ExampleTests {
+
+    @Test
+    public void passedTest() {
+    }
+
+    @Test
+    public void failedTest() {
+	fail("Not yet implemented");
+    }
+
+
+}
+
+
+
---
+++ src/test/java/ParserTests.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ParserTests {
+
+    @Test
+    public void parseEmptyMethod() {
+	fail("Not yet implemented");
+    }
+}
+
+
+
+
+

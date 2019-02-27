26fcb88ffc80f548f34968b7294433d1986e56b5
Jed Chen <jedc101@attu5.cs.washington.edu>
2019-02-26 21:05:44 -0800

    Finished ParserTests.java

--- src/test/java/ParserTests.java
+++ src/test/java/ParserTests.java
@@ -5,13 +5,13 @@ import org.junit.Test;
 
 public class ParserTests {
 
-    private static final String emptyMethod = "void method1() {}";
-    private static final String returnMethod = "int method3() { return 0; }";
-    private static final String parameterMethod = "void method4(int x) { x++; }";
-    private static final String privateMethod = "private void method5() { int a = 5; }";
-    private static final String recursiveMethod = "int method6(int x) {"
-	+ "if (x < 0) return 0; return x + method6(x - 1); }";
-
+    private static final String emptyMethod = "void emptyMethod() {}";
+    private static final String returnMethod = "int returnMethod() { return 0; }";
+    private static final String parameterMethod = "void parameterMethod(int x) { x++; }";
+    private static final String privateMethod = "private void privateMethod() { int a = 5; }";
+    private static final String recursiveMethod = "int recursiveMethod(int x) {"
+	+ "if (x < 0) return 0; return x + recursiveMethod(x - 1); }";
+    private static final String forLoopMethod = "void forLoopMethod() { int x; for (int i = 0; i < 5; i++) { x++; } }";
 
     /*
      * Constructor Tests
@@ -63,22 +63,42 @@ public class ParserTests {
 	parser p = new parser(recursiveMethod);
 	assertEquals(1, p.getAllMethods().size());
 	assertEquals(null, p.getMethod("helloworld"));
-	if (p.getMethod("method6") == null) {
+	if (p.getMethod("recursiveMethod") == null) {
 	    fail("method6 not found in parser");
 	}
     }
 
     @Test
-    public void testTraverse() {
+    public void testEmptyTraverse() {
+	parser p = new parser(emptyMethod);
+	try {
+	    parser.Node root = p.traverse("emptyMethod");
+	    fail("NoSuchElementException should have been thrown");
+	} catch (NoSuchElementException e) { }
+    }
+
+    @Test
+    public void testSimpleTraverse() {
+	parser p = new parser(parameterMethod);
+	parser.Node root = p.traverse("parameterMethod");
+	HashMap<parser.Node, String> map = root.getChildren();
+	assertEquals(0, map.size());
+	assertEquals("x++;", root.getContent());
+    }
+
+    @Test
+    public void testRecursiveTraverse() {
 	parser p = new parser(recursiveMethod);
 	int size = 0;
 	Queue<parser.Node> queue = new LinkedList<parser.Node>();
-	queue.add(p.traverse("method6"));
+	Set<String> lines = new HashSet<String>();
+	queue.add(p.traverse("recursiveMethod"));
 	size++;
 	boolean oneTrue = false;
 	boolean oneFalse = false;
 	do {
 	    parser.Node next = queue.remove();
+	    lines.add(next.getContent());
 	    HashMap<parser.Node, String> map = next.getChildren();
 	    for (parser.Node n : map.keySet()) {
 	        String tag = map.get(n);
@@ -103,9 +123,44 @@ public class ParserTests {
 	    fail("No true tag found");
 	}
 	assertEquals(3, size);
+	assert lines.contains("x < 0");
+	assert lines.contains("return 0;");
+	assert lines.contains("return x + recursiveMethod(x - 1);");
     }
 
-
+    @Test
+    public void testForLoopTraverse() {
+	parser p = new parser(forLoopMethod);
+	int size = 0;
+	Queue<parser.Node> queue = new LinkedList<parser.Node>();
+	Set<String> lines = new HashSet<String>();
+	queue.add(p.traverse("forLoopMethod"));
+	size++;
+	boolean oneTrue = false;
+	do {
+	    parser.Node next = queue.remove();
+	    lines.add(next.getContent());
+	    HashMap<parser.Node, String> map = next.getChildren();
+	    for (parser.Node n : map.keySet()) {
+	        String tag = map.get(n);
+		if (tag.equals("True")) {
+		    if (!oneTrue) {
+			queue.add(n);
+			oneTrue = true;
+		    }
+		} else {
+		    queue.add(n);
+		}
+		size++;
+	    }
+	} while (!queue.isEmpty());
+        assertEquals(5, lines.size());
+	assert lines.contains("int x;");
+	assert lines.contains("int i = 0");
+	assert lines.contains("i < 5");
+	assert lines.contains("i++");
+	assert lines.contains("x++;");
+    }
 }
 
 

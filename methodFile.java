9e324802c60b26115586591a7f5eae82cacdbdb8
CandiceMiao <candicemiao818@gmail.com>
2019-03-03 21:47:55 -0800

    clean up

--- methodFile.java
+++ methodFile.java
@@ -1,14 +1,3 @@
 public class methodFile {
-    public int recur() {
-        int a = 0;
-        a = a + 1;
-        if (a == 1) {
-            a++;
-        } else if (a == 2) {
-            a--;
-        } else {
-            a = a + 3;
-        }
-        return a;
-    }
+int recursiveMethod(int x) {if (x < 0) return 0; return x + recursiveMethod(x - 1); }
 }

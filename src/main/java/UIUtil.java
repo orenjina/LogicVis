8b54ba58b720a8f06dfa93d332684e7bc92f0449
Jed Chen <jedc101@attu1.cs.washington.edu>
2019-03-07 19:05:23 -0800

    Changed Tests to reflect implementation changes

--- src/main/java/UIUtil.java
+++ src/main/java/UIUtil.java
@@ -1,91 +1,107 @@
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.text.*;
 
 // a util class
 public class UIUtil {
-	public static void drawStart(GraphicsContext gc, double pos_x, double pos_y, double scale_x, double scale_y) {
-		gc.strokeOval(pos_x, pos_y, scale_x, scale_y);
-		gc.strokeText("Start", pos_x + 0.25 * scale_x, pos_y + 0.75 * scale_y, 0.5 * scale_x);
-	}
+    private static final Font font = Font.font("Arial", 16);
+    
+    // draws the given statement centered at coordinates (pos_x, pos_y) and returns the width of the text
+    public static void drawText(GraphicsContext gc, String statement, double pos_x, double pos_y) {
+	Text text = new Text(statement);
+	text.setFont(font);
+	double width = text.getLayoutBounds().getWidth();
+	double height = text.getLayoutBounds().getHeight();
+
+        gc.strokeText(statement, pos_x - width / 2, pos_y);
+	gc.setFont(font);
+	System.out.println(statement + " (" + width + ", " + height + ")");
+    }
+
+    public static void drawStart(GraphicsContext gc, double pos_x, double pos_y, double scale_x, double scale_y) {
+	gc.strokeOval(pos_x, pos_y, scale_x, scale_y);
+	// gc.strokeText("Start", pos_x + 0.25 * scale_x, pos_y + 0.75 * scale_y);
+	drawText(gc, "Start", pos_x + scale_x / 2, pos_y + scale_y / 2);
+    }
 	
-	public static void drawConditional(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
-		gc.strokePolygon(new double[] {
-				pos_x, pos_x + 0.5 * scale_x, pos_x + scale_x, pos_x + 0.5 * scale_x
-		}, new double[] {
-				pos_y + 0.5 * scale_y, pos_y, pos_y + 0.5 * scale_y, pos_y + scale_y
-		}, 4);
-		gc.strokeText(statement, pos_x + 0.25 * scale_x, pos_y + 0.75 * scale_y, 0.5 * scale_x);
-	}
+    public static void drawConditional(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
+	gc.strokePolygon(new double[] {
+		pos_x, pos_x + 0.5 * scale_x, pos_x + scale_x, pos_x + 0.5 * scale_x
+	    }, new double[] {
+		pos_y + 0.5 * scale_y, pos_y, pos_y + 0.5 * scale_y, pos_y + scale_y
+	    }, 4);
+	gc.strokeText(statement, pos_x + 0.25 * scale_x, pos_y + 0.75 * scale_y);
+    }
 	
-	public static void drawStatement(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
-		gc.strokeRect(pos_x, pos_y, scale_x, scale_y);
-		gc.strokeText(statement, pos_x + 0.25 * scale_x, pos_y + 0.75 * scale_y, 0.5 * scale_x);
-	}
+    public static void drawStatement(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
+	gc.strokeRect(pos_x, pos_y, scale_x, scale_y);
+	gc.strokeText(statement, pos_x + 0.25 * scale_x, pos_y + 0.75 * scale_y, 0.5 * scale_x);
+    }
 	
-	public static void drawArrow(GraphicsContext gc, double from_x, double from_y, double to_x, double to_y, Boolean down) {
-		gc.strokeLine(from_x, from_y, to_x, to_y);
-		double dir_x = to_x - from_x;
-		double dir_y = to_y - from_y;
-		if (down) {
-			gc.strokeLine(to_x - 0.15 * dir_x + 5, to_y - 0.15 * dir_y, to_x, to_y);
-			gc.strokeLine(to_x - 0.15 * dir_x - 5, to_y - 0.15 * dir_y, to_x, to_y);
-		} else {
-			gc.strokeLine(to_x - 0.15 * dir_x, to_y - 0.15 * dir_y + 5, to_x, to_y);
-			gc.strokeLine(to_x - 0.15 * dir_x, to_y - 0.15 * dir_y - 5, to_x, to_y);
-		}
+    public static void drawArrow(GraphicsContext gc, double from_x, double from_y, double to_x, double to_y, Boolean down) {
+	gc.strokeLine(from_x, from_y, to_x, to_y);
+	double dir_x = to_x - from_x;
+	double dir_y = to_y - from_y;
+	if (down) {
+	    gc.strokeLine(to_x - 0.15 * dir_x + 5, to_y - 0.15 * dir_y, to_x, to_y);
+	    gc.strokeLine(to_x - 0.15 * dir_x - 5, to_y - 0.15 * dir_y, to_x, to_y);
+	} else {
+	    gc.strokeLine(to_x - 0.15 * dir_x, to_y - 0.15 * dir_y + 5, to_x, to_y);
+	    gc.strokeLine(to_x - 0.15 * dir_x, to_y - 0.15 * dir_y - 5, to_x, to_y);
 	}
+    }
 	
-	public static void drawArrowDown(GraphicsContext gc, String statement, double from_x, double from_y, double scale) {
-		drawArrow(gc, from_x, from_y, from_x, from_y + scale, true);
-		gc.strokeText(statement, from_x + 5, from_y + scale / 2 + 10, 100);
-	}
+    public static void drawArrowDown(GraphicsContext gc, String statement, double from_x, double from_y, double scale) {
+	drawArrow(gc, from_x, from_y, from_x, from_y + scale, true);
+	gc.strokeText(statement, from_x + 5, from_y + scale / 2 + 10, 100);
+    }
 	
-	public static void drawArrowRight(GraphicsContext gc, String statement, double from_x, double from_y, double scale) {
-		drawArrow(gc, from_x, from_y, from_x + scale, from_y, false);
-		gc.strokeText(statement, from_x + 5, from_y - 2, 100);
-	}
+    public static void drawArrowRight(GraphicsContext gc, String statement, double from_x, double from_y, double scale) {
+	drawArrow(gc, from_x, from_y, from_x + scale, from_y, false);
+	gc.strokeText(statement, from_x + 5, from_y - 2, 100);
+    }
 	
-	// Must point from left to right
-	public static void drawLine(GraphicsContext gc, String statement, double from_x, double from_y, double to_x, double to_y) {
-		gc.strokeLine(from_x, from_y, to_x, to_y);
-		double dir_x = to_x - from_x;
-		double dir_y = to_y - from_y;
-		gc.strokeText(statement, from_x + 0.25 * dir_x, from_y + 0.75 * dir_y, 0.5 * dir_x);
-	}
+    // Must point from left to right
+    public static void drawLine(GraphicsContext gc, String statement, double from_x, double from_y, double to_x, double to_y) {
+	gc.strokeLine(from_x, from_y, to_x, to_y);
+	double dir_x = to_x - from_x;
+	double dir_y = to_y - from_y;
+	gc.strokeText(statement, from_x + 0.25 * dir_x, from_y + 0.75 * dir_y, 0.5 * dir_x);
+    }
 	
-	public static void drawLineArrow(GraphicsContext gc, String statement, double from_x, double from_y, double to_x, double to_y, double mid_x, double mid_y) {
-		drawLine(gc, statement, Math.min(from_x, mid_x), Math.min(from_y, mid_y), Math.max(from_x, mid_x), Math.max(from_y, mid_y));
-		drawArrow(gc, mid_x, mid_y, to_x, to_y, true);
-	}
+    public static void drawLineArrow(GraphicsContext gc, String statement, double from_x, double from_y, double to_x, double to_y, double mid_x, double mid_y) {
+	drawLine(gc, statement, Math.min(from_x, mid_x), Math.min(from_y, mid_y), Math.max(from_x, mid_x), Math.max(from_y, mid_y));
+	drawArrow(gc, mid_x, mid_y, to_x, to_y, true);
+    }
 	
-	public static void drawParameter(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
-		drawArrow(gc, pos_x - 0.2 * scale_x, pos_y + 0.5 * scale_y, pos_x, pos_y + 0.5 * scale_y, false);
-		drawStatement(gc, statement, pos_x, pos_y, scale_x, scale_y);
-	}
+    public static void drawParameter(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
+	drawArrow(gc, pos_x - 0.2 * scale_x, pos_y + 0.5 * scale_y, pos_x, pos_y + 0.5 * scale_y, false);
+	drawStatement(gc, statement, pos_x, pos_y, scale_x, scale_y);
+    }
 	
-	public static void drawReturn(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
-		drawArrow(gc, pos_x, pos_y + 0.5 * scale_y, pos_x  + 0.2 * scale_x, pos_y + 0.5 * scale_y, false);
-		drawStatement(gc, statement, pos_x, pos_y, scale_x, scale_y);
-	}
+    public static void drawReturn(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
+	drawArrow(gc, pos_x, pos_y + 0.5 * scale_y, pos_x  + 0.2 * scale_x, pos_y + 0.5 * scale_y, false);
+	drawStatement(gc, statement, pos_x, pos_y, scale_x, scale_y);
+    }
 	
-	public static void drawRecurse(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
-		drawStatement(gc, statement, pos_x, pos_y, scale_x, scale_y);
-		//-
-		//-
-		gc.strokeLine(pos_x + scale_x, pos_y + 0.25 * scale_y, pos_x + 1.1 * scale_x, pos_y + 0.25 * scale_y);
-		gc.strokeLine(pos_x + scale_x, pos_y + 0.75 * scale_y, pos_x + 1.1 * scale_x, pos_y + 0.75 * scale_y);
+    public static void drawRecurse(GraphicsContext gc, String statement, double pos_x, double pos_y, double scale_x, double scale_y) {
+	drawStatement(gc, statement, pos_x, pos_y, scale_x, scale_y);
+	//-
+	//-
+	gc.strokeLine(pos_x + scale_x, pos_y + 0.25 * scale_y, pos_x + 1.1 * scale_x, pos_y + 0.25 * scale_y);
+	gc.strokeLine(pos_x + scale_x, pos_y + 0.75 * scale_y, pos_x + 1.1 * scale_x, pos_y + 0.75 * scale_y);
 		
-		// |
-		//-
-		//-
-		// |
-		gc.strokeLine(pos_x + 1.1 * scale_x, pos_y + 0.25 * scale_y, pos_x + 1.1 * scale_x, pos_y + 0.15 * scale_y);
-		gc.strokeLine(pos_x + 1.1 * scale_x, pos_y + 0.75 * scale_y, pos_x + 1.1 * scale_x, pos_y + 0.85 * scale_y);
+	// |
+	//-
+	//-
+	// |
+	gc.strokeLine(pos_x + 1.1 * scale_x, pos_y + 0.25 * scale_y, pos_x + 1.1 * scale_x, pos_y + 0.15 * scale_y);
+	gc.strokeLine(pos_x + 1.1 * scale_x, pos_y + 0.75 * scale_y, pos_x + 1.1 * scale_x, pos_y + 0.85 * scale_y);
 		
-		// |\
-		//-  \
-		//-  /
-		// |/
-		gc.strokeLine(pos_x + 1.1 * scale_x, pos_y + 0.15 * scale_y, pos_x + 1.2 * scale_x, pos_y + 0.5 * scale_y);
-		gc.strokeLine(pos_x + 1.1 * scale_x, pos_y + 0.85 * scale_y, pos_x + 1.2 * scale_x, pos_y + 0.5 * scale_y);
-	}
+	// |\
+	//-  \
+	//-  /
+	// |/
+	gc.strokeLine(pos_x + 1.1 * scale_x, pos_y + 0.15 * scale_y, pos_x + 1.2 * scale_x, pos_y + 0.5 * scale_y);
+	gc.strokeLine(pos_x + 1.1 * scale_x, pos_y + 0.85 * scale_y, pos_x + 1.2 * scale_x, pos_y + 0.5 * scale_y);
+    }
 }

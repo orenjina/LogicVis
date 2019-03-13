e1d0b582a08bda85f3150ab635a16303347daa6a
grz2 <grz2@uw.edu>
2019-03-12 23:00:55 -0700

    week10 submission

--- scripts/initial_results.sh
+++ scripts/initial_results.sh
@@ -1,3 +1,2 @@
 mvn clean test #run tests
-mvn -q clean compile exec:java -Dexec.mainClass=parserClientTest #string representation of testCode
-mvn -q clean compile exec:java -Dexec.mainClass=LogicVisView #basic non-functional UI, but looks good
+mvn -q clean compile exec:java -Dexec.mainClass=parserClientTest #graph representation of testCode
